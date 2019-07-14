0c517d18f2a5a17be7e403bfdd94fbfa832c0d50
Marta Oliver <marta.oliver90@gmail.com>
2019-07-14 18:42:19 +0200

    Cambio librería deprecated en UserController

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,13 +5,13 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use FOS\RestBundle\View\View;
 use AppBundle\Entity\User;
 
-class UserController extends FOSRestController {
+class UserController extends AbstractFOSRestController {
     /**
      * @Rest\Post("/user")
      */
